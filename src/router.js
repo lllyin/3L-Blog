569dc6a4459c79dcee57d80a6fadd22d51746afc
lyin <ling95613@gmail.com>
2018-04-07 19:21:03 +0800

    打包带Hash

--- src/router.js
+++ src/router.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Router, Route, Switch } from 'dva/router';
-import IndexPage from './routes/IndexPage';
 import Home from './routes/Home';
 import Resume from './routes/Resume';
 import Contact from './routes/Contact';
@@ -14,7 +13,7 @@ function RouterConfig({ history }) {
   return (
     <Router history={history}>
       <Switch>
-        <Route path="/" exact component={IndexPage} />
+        <Route path="/" exact component={Home} />
         <Route path="/home" exact component={Home} />
         <Route path="/resume" exact component={Resume} />
         <Route path="/project" exact component={Project} />
