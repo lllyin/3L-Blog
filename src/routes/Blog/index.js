dfe4d3c8ce5ececf57adf27198f4c07a166be2d0
mengke <1148931179@qq.com>
2018-05-11 01:26:43 +0800

    博客路由内容完善

--- src/routes/Blog/index.js
+++ src/routes/Blog/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { ResponsiveHeader } from '../../components/Header';
+import { Abstract } from '../../components/Article';
 import { Content } from '../../components/Layout';
 
 export default class Blog extends Component {
@@ -9,7 +10,10 @@ export default class Blog extends Component {
         <ResponsiveHeader />
         <Content className="home-box" style={{ marginTop: 30 }}>
           <h1> Blog Page </h1>
-          blog page content
+          <Abstract />
+          <Abstract />
+          <Abstract />
+          <Abstract />
         </Content>
       </section>
     );
