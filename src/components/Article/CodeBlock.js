8cfe48d8d4b0563583bb015aff733f2739b4fa35
andesome <ling95613@gmail.com>
2019-03-17 03:54:34 +0800

    chore: 移除hightlight.js

--- src/components/Article/CodeBlock.js
+++ src/components/Article/CodeBlock.js
@@ -1,49 +1,23 @@
-import hljs from 'highlight.js';
 import React from 'react';
 import PropTypes from 'prop-types';
 
-
 export default class CodeBlock extends React.PureComponent {
-  constructor(props) {
-    super(props);
-    this.setRef = this.setRef.bind(this);
-  }
-
-  componentDidMount() {
-    this.highlightCode();
-  }
-
-  componentDidUpdate() {
-    this.highlightCode();
-  }
-
-  setRef(el) {
-    this.codeEl = el;
-  }
-
-
-  highlightCode() {
-    hljs.highlightBlock(this.codeEl);
-  }
-
   render() {
-    console.log('codeBlock', this.props);
+    const { language, value } = this.props;
+
     return (
       <pre>
-        <code ref={this.setRef} className={`language-${this.props.language}`}>
-          {this.props.value}
-        </code>
+        <code className={`language-${language}`}>{value}</code>
       </pre>
     );
   }
 }
 
 CodeBlock.defaultProps = {
-  language: 'js',
+  language: 'js'
 };
 
 CodeBlock.propTypes = {
   value: PropTypes.string.isRequired,
-  language: PropTypes.string,
+  language: PropTypes.string
 };
-
