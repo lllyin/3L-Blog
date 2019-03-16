fffb8e8782637db42de49148c325dd4982ff5f1c
andesome <ling95613@gmail.com>
2019-03-17 03:18:33 +0800

    feat: 替换antd Modal

--- src/routes/Blog/BlogDetail.js
+++ src/routes/Blog/BlogDetail.js
@@ -1,13 +1,15 @@
-import React, { Component } from "react";
-import qs from "qs";
-import { Modal } from "antd";
-import { connect } from "dva";
-import { ResponsiveHeader } from "../../components/Header";
-import Article from "../../components/Article";
-import { Content } from "../../components/Layout";
-import { Comment } from "../../components/Comment";
-import { jumpToLogin } from "../../services/api";
-import styles from "./blogDetail.less";
+import React, { Component } from 'react';
+import qs from 'qs';
+import { connect } from 'dva';
+import { ResponsiveHeader } from '../../components/Header';
+import Article from '../../components/Article';
+import { Content } from '../../components/Layout';
+import { Comment } from '../../components/Comment';
+import Dialog from '../../components/Dialog';
+import { jumpToLogin } from '../../services/api';
+import styles from './blogDetail.less';
+
+const { confirm } = Dialog;
 
 @connect(({ article, user, loading }) => ({
   article,
@@ -25,26 +27,27 @@ export default class BlogDetail extends Component {
   componentDidMount() {
     const { dispatch } = this.props;
     const { args } = this.state;
+
     dispatch({
-      type: "article/fetch"
+      type: 'article/fetch'
     });
     dispatch({
-      type: "article/fetchDetail",
+      type: 'article/fetchDetail',
       articleId: args.id
     });
   }
 
   showConfirm = () => {
-    Modal.confirm({
-      title: "登录后方可评论",
-      content: "我们一起去登录好吗，不需要手机，只需要填个昵称和密码就好了，大佬 ^_^",
-      okText: "走!去登录",
-      cancelText: "再等会",
+    confirm({
+      title: '登录后方可评论',
+      content: '我们一起去登录好吗，不需要手机，只需要填个昵称和密码就好了，大佬 ^_^',
+      okText: '走!去登录',
+      cancelText: '再等会',
       onOk() {
         jumpToLogin();
       },
       onCancel() {
-        console.log("Cancel");
+        console.log('Cancel');
       }
     });
   };
@@ -56,7 +59,8 @@ export default class BlogDetail extends Component {
       user: { userInfo },
       dispatch
     } = this.props;
-    console.log("提交评论", content, article.detail);
+
+    console.log('提交评论', content, article.detail);
     if (!article.detail._id) {
       return;
     }
@@ -65,11 +69,11 @@ export default class BlogDetail extends Component {
       return false;
     }
     return dispatch({
-      type: "article/addComment",
+      type: 'article/addComment',
       postId: article.detail._id,
       data: {
         from: userInfo.name,
-        to: "",
+        to: '',
         content
       }
     });
@@ -78,20 +82,31 @@ export default class BlogDetail extends Component {
   // 处理点赞
   handleLike = commentObj => {
     const { dispatch } = this.props;
+
     return dispatch({
-      type: "article/likeAComment",
+      type: 'article/likeAComment',
       commentId: commentObj._id
     });
   };
 
   render() {
     const { article, loading } = this.props;
+
     return (
       <section>
         <ResponsiveHeader />
-        <Content className={`home-box ${styles["blog-detail"]}`} style={{ marginTop: 30 }} loading={loading.models.article}>
+        <Content
+          className={`home-box ${styles['blog-detail']}`}
+          style={{ marginTop: 30 }}
+          loading={loading.models.article}
+        >
           <Article data={article.detail} {...this.props} />
-          <Comment list={article.detail.comments} onComment={this.handleComment} onLike={this.handleLike} loading={loading.effects["article/addComment"]} />
+          <Comment
+            list={article.detail.comments}
+            onComment={this.handleComment}
+            onLike={this.handleLike}
+            loading={loading.effects['article/addComment']}
+          />
         </Content>
       </section>
     );
