ac795deaa2e019e742bcafde0e7448903f8d6e73
andesome <ling95613@gmail.com>
2019-03-17 12:20:22 +0800

    chore: 移除antd spine组件

--- src/components/Layout/Content.js
+++ src/components/Layout/Content.js
@@ -1,15 +1,47 @@
 import React from 'react';
-import { Spin } from 'antd';
+import green from '@material-ui/core/colors/green';
+import classNames from 'classnames';
+import Fade from '@material-ui/core/Fade';
+import { withStyles } from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import styles from './Content.less';
 
-const Content = ({ children, className, loading = false, ...rest }) => (
-  <Spin spinning={loading}>
-    <div className={`${styles.content} ${className}`} {...rest}>
-      {children}
-    </div>
-  </Spin>
+const jss = theme => ({
+  root: {
+    display: 'flex',
+    alignItems: 'center'
+  },
+  wrapper: {
+    margin: theme.spacing.unit,
+    position: 'relative'
+  },
+  loading: {
+    opacity: 0.5,
+    filter: 'blur(1px)'
+  },
+  buttonSuccess: {
+    backgroundColor: green[500],
+    '&:hover': {
+      backgroundColor: green[700]
+    }
+  },
+  buttonProgress: {
+    // color: green[500],
+    position: 'absolute',
+    top: '20vh',
+    left: '50%',
+    marginTop: -12,
+    marginLeft: -12
+  }
+});
 
-  );
+const Content = ({ children, className, classes, loading = false, ...rest }) => (
+  <Fade in className={classNames({ [classes.loading]: loading })}>
+    <section className={classNames(`${styles.content} ${className}`, classes.wrapper)} {...rest}>
+      <div>{children}</div>
+      {loading && <CircularProgress size={24} className={classes.buttonProgress} />}
+    </section>
+  </Fade>
+);
 
-
-export default Content;
+export default withStyles(jss)(Content);
